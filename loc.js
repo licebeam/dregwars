2228504b285f4615727a901da88304eed3016ab7
licebeam <dustinwalkerart@gmail.com>
2018-03-08 23:36:18 +0900

    adjusted css, added more to loc.js added new dom containers

--- loc.js
+++ loc.js
@@ -1,23 +1,31 @@
-class locClass {
+class locClass { //base location class, contains a lot of logic
     constructor(name, desc, sear) {
 
         this.name = name;
         this.desc = desc;
-        this.sear = sear;
+
     }
     randomEvent() { //random event method
-        return "poop";
+        return eventResult[Math.floor(Math.random() * (1 - 0 + 1)) + 0]
     };
+
+    searchEvent() { //random search event for event
+        return searchResult[Math.floor(Math.random() * (1 - 0 + 1)) + 0]
+    }
+
+    dealerEvent() { //random search event for dealer
+        return dealerResult[Math.floor(Math.random() * (2 - 0 + 1)) + 0]
+    }
 }
 
+//inhereted classes
 var loc1 = new locClass;
 loc1.name = "Downtown";
 loc1.desc = "This is Downtown.";
-loc1.sear = ""
 
 var loc2 = new locClass;
 loc2.name = "East Side";
-loc2.desc = "A smelly dump of a town.";
+loc2.desc = "Corporate office district";
 
 var loc3 = new locClass;
 loc3.name = "West Side";
@@ -26,3 +34,8 @@ loc3.desc = "The slums of Detroit.";
 var loc4 = new locClass;
 loc4.name = "South End";
 loc4.desc = "The proverbial end of the city.";
+
+//data structures for random events
+var searchResult = ["You found nothing", "You found something"];
+var eventResult = ["It's Sunny Out", "It's Raining"];
+var dealerResult = ["Cyber", "Dreg", "Gear"];
--- main.js
+++ main.js
@@ -4,33 +4,37 @@ var locationText = document.getElementById("loc");
 var locationDes = document.getElementById("locDescription");
 
 var descriptText = document.getElementById("descriptText");
-
-//MAIN
+var eventText = document.getElementById("eventArea");
+//MAIN Button menu
 function button(obj, id) {
 
     switch (id) {
         case btn1:
             locationText.innerText = "Detroit: " + loc1.name;
             locationDes.innerText = "Description: " + loc1.desc;
+            eventText.innerText = loc1.randomEvent();
             break;
 
         case btn2:
             locationText.innerText = "Detroit: " + loc2.name;
             locationDes.innerText = "Description: " + loc2.desc;
+            eventText.innerText = loc1.randomEvent();
             break;
 
         case btn3:
             locationText.innerText = "Detroit: " + loc3.name;
             locationDes.innerText = "Description: " + loc3.desc;
+            eventText.innerText = loc1.randomEvent();
             break;
 
         case btn4:
             locationText.innerText = "Detroit: " + loc4.name;
             locationDes.innerText = "Description: " + loc4.desc;
+            eventText.innerText = loc1.randomEvent();
             break;
 
         case btnSe:
-            descriptText.innerText = loc1.randomEvent();
+            descriptText.innerText = loc1.searchEvent();
 
             break;
 
