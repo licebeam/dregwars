5f12a1ef05fb694566ccc0b16745ca1dcff1a8a9
licebeam <dustinwalkerart@gmail.com>
2018-03-10 13:05:38 +0900

    basic inventory and buy system

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var locationDes = document.getElementById("locDescription");
 var descriptText = document.getElementById("descriptText");
 var eventText = document.getElementById("eventArea");
 
+var invenPlayer = document.getElementById("playerInventory");
 
 function hideItems() {
     document.getElementById("buyItem1").style.visibility = "hidden";
@@ -16,6 +17,36 @@ function hideItems() {
 function showItems() {
     document.getElementById("buyItem1").style.visibility = "visible";
     document.getElementById("buyItem2").style.visibility = "visible";
+}
+
+
+//player Inventory object
+var playerInv = [{
+    name: "Credit",
+    amount: 1
+}, {
+    name: "Cyber Wares",
+    amount: 1
+}]
+
+//get buy names for below function----
+var buyName1 = document.getElementById("buyName1").textContent;
+//function for buying item
+function buyItems(name, amt) {
+    for (i = 0; i < playerInv.length; i++) {
+        if (playerInv[i].name == name) {
+            playerInv[i].amount += amt;
+        }
+    }
+}
+
+function updateInv() {
+    invenPlayer.innerText = "";
+    for (i = 0; i < playerInv.length; i++) {
+        invenPlayer.innerText += "\n Name: " + playerInv[i].name;
+        invenPlayer.innerText += " Owned: " + playerInv[i].amount;
+    }
+
 }
 //MAIN Button menu
 function button(obj, id) {
@@ -56,5 +87,21 @@ function button(obj, id) {
             showItems();
             break;
 
+
+            ///buttons for item buys
+        case buyBtn1:
+            buyItems(buyName1, 1);
+            alert("you bought " + buyName1 + " Owned: " + playerInv[0].amount);
+            updateInv();
+            break;
+
+        case sellBtn1:
+            alert("you sold somethin");
+            alert(JSON.stringify(playerInv))
+            break;
+
     }
 };
+
+//populate Player inventory.
+updateInv();
